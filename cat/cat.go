c6f8ffebb5006781ca52f48c281218196e3398e3
Chad Clark <chad-git-p.superfrink@superfrink.net>
2020-02-28 16:05:42 -0700

    Add 'cat' flags: -u, -E, -T. Add 'cat' reading from STDIN when no filenames are specified. Add 'cat' -b overriding -n .

--- cat/cat.go
+++ cat/cat.go
@@ -6,18 +6,30 @@
 //
 package main
 
-import "bufio"
-import "flag"
-import "fmt"
-import "io"
-import "net"
-import "os"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+)
 
 const (
 	help_text string = `
     Usage: cat [OPTIONS] [FILE]...
-    
-    concatenate and print the content of files
+
+    Concatenate and print the content of files.
+
+    With no FILE, or when FILE is -, read standard input.
+
+        -b, --number-nonblank    number nonempty output lines, overrides -n
+        -E, --show-ends          display $ at end of each line
+        -n, --number             number all output lines
+        -s, --squeeze-blank      suppress repeated empty output lines
+        -T, --show-tabs          display TAB characters as ^I
+        -u                       (ignored)
 
         --help        display this help and exit
         --version     output version information and exit
@@ -27,15 +39,23 @@ const (
 
     Copyright (C) 2014, The GO-Coreutils Developers.
     This program comes with ABSOLUTELY NO WARRANTY; for details see
-    LICENSE. This is free software, and you are welcome to redistribute 
+    LICENSE. This is free software, and you are welcome to redistribute
     it under certain conditions in LICENSE.
 `
 )
 
 var (
-	countNonBlank     = flag.Bool("b", false, "Number the non-blank output lines, starting at 1.")
-	numberOutput      = flag.Bool("n", false, "Number the output lines, starting at 1.")
-	squeezeEmptyLines = flag.Bool("s", false, "Squeeze multiple adjacent empty lines, causing the output to be single spaced.")
+	countNonBlank         = flag.Bool("b", false, "Number the non-blank output lines, starting at 1.")
+	countNonBlankLong     = flag.Bool("number-nonblank", false, "Number the non-blank output lines, starting at 1.")
+	ignoredU              = flag.Bool("u", false, "(ignored)")
+	numberOutput          = flag.Bool("n", false, "Number the output lines, starting at 1.")
+	showEnds              = flag.Bool("E", false, "Display $ at end of each line.")
+	showEndsLong          = flag.Bool("show-ends", false, "Display $ at end of each line.")
+	showEndsNonPrint      = flag.Bool("e", false, "Equivalent to -vE")
+	showTabs              = flag.Bool("T", false, "Display TAB characters as ^I")
+	showTabsLong          = flag.Bool("show-tabs", false, "Display TAB characters as ^I")
+	squeezeEmptyLines     = flag.Bool("s", false, "Squeeze multiple adjacent empty lines, causing the output to be single spaced.")
+	squeezeEmptyLinesLong = flag.Bool("squeeze-blank", false, "Squeeze multiple adjacent empty lines, causing the output to be single spaced.")
 )
 
 func openFile(s string) (io.ReadWriteCloser, error) {
@@ -60,6 +80,12 @@ func dumpLines(w io.Writer, r io.Reader) (n int64, err error) {
 		}
 		if *squeezeEmptyLines && lastline == "\n" && line == "\n" {
 			continue
+		}
+		if *showEnds {
+			line = strings.ReplaceAll(line, "\n", "$\n")
+		}
+		if *showTabs {
+			line = strings.ReplaceAll(line, "\t", "^I")
 		}
 		if *countNonBlank && line == "\n" || line == "" {
 			fmt.Fprint(w, line)
@@ -89,23 +115,52 @@ func main() {
 		os.Exit(0)
 	}
 
-	rcopy := io.Copy
-	if *countNonBlank || *numberOutput || *squeezeEmptyLines {
-		rcopy = dumpLines
+	// process the long versions of the flags
+	if *countNonBlankLong {
+		*countNonBlank = true
+	}
+	if *showEndsLong {
+		*showEnds = true
+	}
+	if *showEndsNonPrint {
+		*showEnds = true
+		// FIXME enable show-non-printing, ie -v
+	}
+	if *showTabsLong {
+		*showTabs = true
+	}
+	if *squeezeEmptyLinesLong {
+		*squeezeEmptyLines = true
 	}
 
-	for _, fname := range flag.Args() {
+	// -b overrides -n
+	if *countNonBlank {
+		*numberOutput = false
+	}
+
+	catenatingFunc := io.Copy
+	// some flags cause the input data to be modified before being output
+	if *countNonBlank || *numberOutput || *squeezeEmptyLines || *showEnds || *showTabs {
+		catenatingFunc = dumpLines
+	}
+
+	fileList := flag.Args()
+	// read from stdin if no files were specified
+	if 0 == len(fileList) {
+		fileList = append(fileList, "-")
+	}
+
+	for _, fname := range fileList {
 		if fname == "-" {
-			rcopy(os.Stdout, os.Stdin)
+			catenatingFunc(os.Stdout, os.Stdin)
 		} else {
 			f, err := openFile(fname)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
 				continue
 			}
-			rcopy(os.Stdout, f)
+			catenatingFunc(os.Stdout, f)
 			f.Close()
 		}
 	}
-
 }
